e634956a71e98c31c83812b7989eb312ca303eaa
aleksandar <jocovic099@gmail.com>
2020-09-20 23:52:18 +0200

    Project info btn updated and project animations.

--- slide.js
+++ slide.js
@@ -408,3 +408,20 @@ const removePageNavAnimation = () => {
   }, 300)
 }
 
+
+// ligt/dark
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// menu navigationnnnananananan
